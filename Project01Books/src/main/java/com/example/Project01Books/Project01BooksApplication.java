7df9f50234d95687001fa3d7ca50395c62464cc9
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-28 14:59:27 -0300

    Project files for Project01Books

--- Project01Books/src/main/java/com/example/Project01Books/Project01BooksApplication.java
+++ Project01Books/src/main/java/com/example/Project01Books/Project01BooksApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Project01BooksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project01BooksApplication.class, args);
+        SpringApplication.run(Project01BooksApplication.class, args);
 	}
 
 }
