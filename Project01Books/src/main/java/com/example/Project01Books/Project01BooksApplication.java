0f777a2eb1dc6f26d05009d13321a2f3858ae43e
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-28 10:04:28 -0300

    Project01 starter

---
+++ Project01Books/src/main/java/com/example/Project01Books/Project01BooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project01Books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01BooksApplication.class, args);
+	}
+
+}
---
+++ Project01Books/src/test/java/com/example/Project01Books/Project01BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project01Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
