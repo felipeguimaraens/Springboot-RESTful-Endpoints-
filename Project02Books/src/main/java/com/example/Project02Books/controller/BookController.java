e42d65322c1c736b2f998b0e66a8a8f83cbb0e06
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-29 13:51:55 -0300

    Modified Delete, PUT and GET to use Id instead of Title

--- Project02Books/src/main/java/com/example/Project02Books/controller/BookController.java
+++ Project02Books/src/main/java/com/example/Project02Books/controller/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public Book getBookById(@PathVariable int id) {
+    public Book getBookById(@PathVariable long id) {
         return books.stream()
                 .filter(book -> book.getId() == id)
                 .findFirst()
@@ -46,19 +46,19 @@ public class BookController {
         }
     }
 
-    @PutMapping("/{title}")
-    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable long id, @RequestBody Book updatedBook) {
         for (int i = 0; i < books.size() ; i++) {
-            if (books.get(i).getTitle().equalsIgnoreCase(title)) {
+            if (books.get(i).getId() == id) {
                 books.set(i, updatedBook);
                 return ;
             }
         }
     }
 
-    @DeleteMapping("/{title}")
-    public void deleteBook(@PathVariable String title) {
-        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable long id) {
+        books.removeIf(book -> book.getId() == id);
     }
 
     private void initializeBooks() {
