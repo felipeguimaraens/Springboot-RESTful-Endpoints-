1952433e1603af8687978fc016ecb7527750724c
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-30 20:17:07 -0300

    Added documentation for Swagger

--- Project02Books/src/main/java/com/example/Project02Books/controller/BookController.java
+++ Project02Books/src/main/java/com/example/Project02Books/controller/BookController.java
@@ -2,6 +2,9 @@ package com.example.Project02Books.controller;
 
 import com.example.Project02Books.entity.Book;
 import com.example.Project02Books.entity.BookRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.http.HttpStatus;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Tag(name="SpringBooks - Books API", description="Collection of APIs to operate books")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -19,16 +23,20 @@ public class BookController {
         initializeBooks();
     }
 
+    @Operation(summary="Get book by id", description="Retrieve a book by id")
     @GetMapping("/{id}")
-    public Book getBookById(@PathVariable @Min(value = 0) long id) {
+    public Book getBookById(@Parameter(description="Optional query parameter")
+                                @PathVariable @Min(value = 0) long id) {
         return books.stream()
                 .filter(book -> book.getId() == id)
                 .findFirst()
                 .orElse(null);
     }
 
+    @Operation(summary="Get books by category", description="Get all books within same category")
     @GetMapping
-    public List<Book> getBooks(@RequestParam(required = false) String category) {
+    public List<Book> getBooks(@Parameter(description="Category of book to be retrieved")
+                                   @RequestParam(required = false) String category) {
         if (category == null) {
             return books;
         }
@@ -38,6 +46,7 @@ public class BookController {
                 .toList();
     }
 
+    @Operation(summary="Create a book", description="Create a new book")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void createBook(@RequestBody BookRequest bookRequest) {
@@ -48,8 +57,10 @@ public class BookController {
         books.add(book);
     }
 
+    @Operation(summary="Update book", description="Update a book by id")
     @PutMapping("/{id}")
-    public void updateBook(@PathVariable @Min(value = 0) long id, @RequestBody BookRequest bookRequest) {
+    public void updateBook(@Parameter(description="Id of book to be updated")
+            @PathVariable @Min(value = 0) long id, @RequestBody BookRequest bookRequest) {
         for (int i = 0; i < books.size() ; i++) {
             if (books.get(i).getId() == id) {
                 Book updatedBook = convertToBook(id, bookRequest);
@@ -59,12 +70,15 @@ public class BookController {
         }
     }
 
+    @Operation(summary="Delete book", description="Delete a book by id")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable @Min(value = 0) long id) {
+    public void deleteBook(@Parameter(description="Id of book to be deleted")
+            @PathVariable @Min(value = 0) long id) {
         books.removeIf(book -> book.getId() == id);
     }
 
+    @Operation(summary="404 dummy page", description="Just return 404 as test")
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @GetMapping("/resource-not-found")
     public void resourceNotFound() {
