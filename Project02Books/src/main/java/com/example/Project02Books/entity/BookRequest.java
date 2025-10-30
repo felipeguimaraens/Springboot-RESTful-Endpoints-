fa3d83c0fcadb370ad134a5c9f70432b5b32f6d3
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-30 15:31:53 -0300

    added constraints to BookRequest fields

--- Project02Books/src/main/java/com/example/Project02Books/entity/BookRequest.java
+++ Project02Books/src/main/java/com/example/Project02Books/entity/BookRequest.java
@@ -1,9 +1,16 @@
 package com.example.Project02Books.entity;
 
+import jakarta.validation.constraints.*;
+
 public class BookRequest {
+    @Size(min=1, max=30)
     private String title;
+    @Size(min=1, max=40)
     private String author;
+    @Size(min=1, max=30)
     private String category;
+    @Min(value=1)
+    @Max(value=5)
     private int rating;
 
     public BookRequest(String title, String author, String category, int rating) {
