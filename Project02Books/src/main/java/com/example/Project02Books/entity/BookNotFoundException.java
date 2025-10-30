1f814c3aa762f3efe8979cf29e8c21806cfc931e
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-30 20:23:58 -0300

    Created BookNotFoundException

---
+++ Project02Books/src/main/java/com/example/Project02Books/entity/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.Project02Books.entity;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+    
+}
