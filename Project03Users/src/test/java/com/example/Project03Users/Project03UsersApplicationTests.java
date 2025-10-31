6105356a6515648def952361b41acd4c67c8836b
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-31 08:38:05 -0300

    Recreated folder for project03 with dependencies

---
+++ Project03Users/src/test/java/com/example/Project03Users/Project03UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project03Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project03UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
