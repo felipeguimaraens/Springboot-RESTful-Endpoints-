2793312ecc1bfe4ae98bfedd153b1998e1cdf34f
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-04 14:01:28 -0300

    Added swagger authentication

--- Project03Users/src/main/java/com/example/Project03Users/security/SecurityConfig.java
+++ Project03Users/src/main/java/com/example/Project03Users/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.Project03Users.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,17 +42,34 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers("/docs/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/h2-console/**").permitAll()
+                        .requestMatchers("/docs/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN"));
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
+        http.httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable());
         http.httpBasic(Customizer.withDefaults());
         // Not required for REST APIs but recommended for HTML forms
         http.csrf(csrf -> csrf.disable());
 
+        http.exceptionHandling(exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(authenticationEntryPoint()));
+
     return http.build();
     }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setHeader("WWW-Authenticate", "");
+            response.getWriter().write("{\"error\": \"Anauthorized access\"}");
+        };
+    }
+
 }
