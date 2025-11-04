b5b8c8e7574a1fb8c2409d417aacdca8f3b63798
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-04 16:33:12 -0300

    Created entities Todo and User

---
+++ Project04Todos/src/main/java/com/example/Project04Todos/entity/Todo.java
@@ -0,0 +1,74 @@
+package com.example.Project04Todos.entity;
+
+import jakarta.persistence.*;
+
+@Table(name="todo")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int priority;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+    public Todo() {}
+
+    public Todo(String title, String description, int priority, boolean complete) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
