4d8419a383e5e9feaeeeff8a131e8cfe1732e272
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-04 15:35:01 -0300

    Created folder for new project

---
+++ Project04Todos/src/main/java/com/example/Project04Todos/Project04TodosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project04Todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project04TodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project04TodosApplication.class, args);
+	}
+
+}
