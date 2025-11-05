b7ddfd9d0729c949b2faa7971fe838f5dcf50408
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-05 13:28:05 -0300

    Added login method

--- Project04Todos/src/main/java/com/example/Project04Todos/service/AuthenticationServiceImpl.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/service/AuthenticationServiceImpl.java
@@ -3,22 +3,32 @@ package com.example.Project04Todos.service;
 import com.example.Project04Todos.entity.Authority;
 import com.example.Project04Todos.entity.User;
 import com.example.Project04Todos.repository.UserRepository;
+import com.example.Project04Todos.request.AuthenticationRequest;
 import com.example.Project04Todos.request.RegisterRequest;
+import com.example.Project04Todos.response.AuthenticationResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                                     AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -30,6 +40,21 @@ public class AuthenticationServiceImpl implements AuthenticationService{
         User user = buildNewUser(input);
         //assignAuthorities(user);
         userRepository.save(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
+
+        String jwtToken = jwtService.generateToken(new HashMap<>(), user);
+
+        return new AuthenticationResponse(jwtToken);
     }
 
     private boolean isEmailTaken(String email) {
