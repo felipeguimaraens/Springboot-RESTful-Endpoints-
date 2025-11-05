82d2b559c0c748fa6059c646c1f77e30a0e70797
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-05 14:41:14 -0300

    Created SwaggerConfig

--- Project04Todos/src/main/java/com/example/Project04Todos/config/SecurityConfig.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/config/SecurityConfig.java
@@ -60,7 +60,11 @@ public class SecurityConfig {
         http.authorizeHttpRequests(configurer ->
                 configurer
                         .requestMatchers("/api/auth/**","/swagger-ui/**", "/v3/api-docs/**",
-                                "/swagger-resources/**", "/webjars/**", "/docs").permitAll());
+                                "/swagger-resources/**", "/webjars/**", "/docs")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+        );
         http.csrf(csrf -> csrf.disable());
         http.exceptionHandling(exceptionHandling ->
                 exceptionHandling
