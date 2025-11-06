769cc0d3bf900f1c09f016e0c5cb5a72162a8318
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-06 09:02:43 -0300

    Created TodoService

--- Project04Todos/src/main/java/com/example/Project04Todos/repository/TodoRepository.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/repository/TodoRepository.java
@@ -1,9 +1,9 @@
 package com.example.Project04Todos.repository;
 
-import com.example.Project04Todos.entity.User;
+import com.example.Project04Todos.entity.Todo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends CrudRepository<User, Long> {
+public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
--- Project04Todos/src/main/java/com/example/Project04Todos/service/TodoServiceImpl.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/service/TodoServiceImpl.java
@@ -1,15 +1,48 @@
 package com.example.Project04Todos.service;
 
+import com.example.Project04Todos.entity.Todo;
+import com.example.Project04Todos.entity.User;
+import com.example.Project04Todos.repository.TodoRepository;
 import com.example.Project04Todos.request.TodoRequest;
 import com.example.Project04Todos.response.TodoResponse;
+import com.example.Project04Todos.util.FindAuthenticatedUser;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TodoServiceImpl implements TodoService{
+    private final TodoRepository todoRepository;
+    private final FindAuthenticatedUser findAuthenticatedUser;
+
+    public TodoServiceImpl(TodoRepository todoRepository, FindAuthenticatedUser findAuthenticatedUser) {
+        this.todoRepository = todoRepository;
+        this.findAuthenticatedUser = findAuthenticatedUser;
+    }
 
     @Override
+    @Transactional
     public TodoResponse createTodo(TodoRequest todoRequest) {
-        return null;
+        User currentUser = findAuthenticatedUser.getAuthenticatedUser();
+
+        Todo todo = new Todo(
+                todoRequest.getTitle(),
+                todoRequest.getDescription(),
+                todoRequest.getPriority(),
+                false,
+                currentUser
+        );
+
+        Todo savedTodo = todoRepository.save(todo);
+
+        TodoResponse todoResponse = new TodoResponse(
+                savedTodo.getId(),
+                savedTodo.getTitle(),
+                savedTodo.getDescription(),
+                savedTodo.getPriority(),
+                savedTodo.isComplete()
+        );
+
+        return todoResponse;
     }
 
 }
