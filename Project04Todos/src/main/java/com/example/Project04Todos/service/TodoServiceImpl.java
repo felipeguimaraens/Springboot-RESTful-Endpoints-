619d47b6bef97c4314b346c8311cbd8d1afb8fc3
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-06 09:40:27 -0300

    Added toggleTodo

--- Project04Todos/src/main/java/com/example/Project04Todos/service/TodoServiceImpl.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/service/TodoServiceImpl.java
@@ -6,10 +6,13 @@ import com.example.Project04Todos.repository.TodoRepository;
 import com.example.Project04Todos.request.TodoRequest;
 import com.example.Project04Todos.response.TodoResponse;
 import com.example.Project04Todos.util.FindAuthenticatedUser;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoServiceImpl implements TodoService{
@@ -25,7 +28,7 @@ public class TodoServiceImpl implements TodoService{
     @Transactional(readOnly = true)
     public List<TodoResponse> getAllTodos() {
         User currentUser = findAuthenticatedUser.getAuthenticatedUser();
-        
+
         return todoRepository.findByOwner(currentUser)
                 .stream()
                 .map(this::convertToTodoResponse)
@@ -48,6 +51,22 @@ public class TodoServiceImpl implements TodoService{
         Todo savedTodo = todoRepository.save(todo);
 
         return convertToTodoResponse(savedTodo);
+    }
+
+    @Override
+    @Transactional
+    public TodoResponse toggleTodoCompletion(long id) {
+        User currentUser = findAuthenticatedUser.getAuthenticatedUser();
+        Optional<Todo> todo = todoRepository.findByIdAndOwner(id, currentUser);
+
+        if (todo.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "To-do not found");
+        }
+
+        todo.get().setComplete(!todo.get().isComplete());
+        Todo updatedTodo = todoRepository.save(todo.get());
+
+        return convertToTodoResponse(updatedTodo);
     }
 
     private TodoResponse convertToTodoResponse(Todo todo) {
