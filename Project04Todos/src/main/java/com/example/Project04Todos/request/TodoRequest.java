f104bffb60d969cb53f3acd45ac65295ca4c4e6e
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-06 08:51:27 -0300

    Added data validation

--- Project04Todos/src/main/java/com/example/Project04Todos/request/TodoRequest.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/request/TodoRequest.java
@@ -1,8 +1,21 @@
 package com.example.Project04Todos.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class TodoRequest {
+
+    @NotEmpty(message = "Title is mandatory")
+    @Size(min = 3, max = 30, message = "Title must be at least 3 characters long")
     private String title;
+    @NotEmpty(message = "Description is mandatory")
+    @Size(min = 3, max = 30, message = "Description must be at least 3 characters long")
     private String description;
+    @NotEmpty(message = "Priority is mandatory")
+    @Min(1)
+    @Max(5)
     private int priority;
 
     public TodoRequest(String title, String description, int priority) {
