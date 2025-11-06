27d9af7ce00d593e5dd8cf012f1224cedf4570ba
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-11-06 09:49:20 -0300

    Added new TODO operations

--- Project04Todos/src/main/java/com/example/Project04Todos/controller/TodoController.java
+++ Project04Todos/src/main/java/com/example/Project04Todos/controller/TodoController.java
@@ -6,8 +6,11 @@ import com.example.Project04Todos.service.TodoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "TO-OD REST API", description = "Operations for managing user to-dos")
 @RestController
@@ -25,4 +28,27 @@ public class TodoController {
     public TodoResponse createTodo(@Valid @RequestBody TodoRequest todoRequest) {
         return todoService.createTodo(todoRequest);
     }
+
+    @Operation(summary = "Get all TO-DOs", description = "Fetch all TO-DOs for current user")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<TodoResponse> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @Operation(summary = "Toggle TO-DO completition", description = "Update current user TO-DOs")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public TodoResponse toggleTodoCompletion(@PathVariable @Min(1) long id) {
+        return todoService.toggleTodoCompletion(id);
+    }
+
+    @Operation(summary = "Delete todo for user", description = "Delete todo for the signed in user")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable @Min(1) long id) {
+        todoService.deleteTodo(id);
+    }
+
+
 }
